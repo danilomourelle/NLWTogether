32fab3173ee10ecf42e91d8e2ee463ccb700677d
Danilo Mourelle <danilo.mourelle@outlook.com.br>
2021-06-22 21:06:54 -0300

    Aula 2 - TypeORM (config, migrations, CustomRepos, Entities) - Criando Usuario

---
+++ src/entities/User.ts
@@ -0,0 +1,30 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+@Entity("USERS")
+export class User {
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn({ name: "created_at" })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: "updated_at" })
+    updatedAt: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+
+}
